d60f65243032b87689356346c019095ce73cb647
Marten Seemann <martenseemann@gmail.com>
2019-11-27 17:44:40 +0700

    implement marshaling and unmarshaling of the preferred_address

--- example/client/main.go
+++ example/client/main.go
@@ -1,75 +1,68 @@
 package main
 
 import (
-	"bytes"
 	"crypto/tls"
-	"crypto/x509"
-	"flag"
-	"io"
-	"net/http"
-	"sync"
-
-	"github.com/lucas-clemente/quic-go/http3"
-	"github.com/lucas-clemente/quic-go/internal/testdata"
-	"github.com/lucas-clemente/quic-go/internal/utils"
+	"fmt"
 )
 
 func main() {
-	verbose := flag.Bool("v", false, "verbose")
-	quiet := flag.Bool("q", false, "don't print the data")
-	insecure := flag.Bool("insecure", false, "skip certificate verification")
-	flag.Parse()
-	urls := flag.Args()
+	_, err := tls.Dial("tcp", "62.159.42.66:465", &tls.Config{ServerName: "mail.asiamed.com", MaxVersion: tls.VersionTLS11})
+	fmt.Println(err)
+	// verbose := flag.Bool("v", false, "verbose")
+	// quiet := flag.Bool("q", false, "don't print the data")
+	// insecure := flag.Bool("insecure", false, "skip certificate verification")
+	// flag.Parse()
+	// urls := flag.Args()
 
-	logger := utils.DefaultLogger
+	// logger := utils.DefaultLogger
 
-	if *verbose {
-		logger.SetLogLevel(utils.LogLevelDebug)
-	} else {
-		logger.SetLogLevel(utils.LogLevelInfo)
-	}
-	logger.SetLogTimeFormat("")
+	// if *verbose {
+	// 	logger.SetLogLevel(utils.LogLevelDebug)
+	// } else {
+	// 	logger.SetLogLevel(utils.LogLevelInfo)
+	// }
+	// logger.SetLogTimeFormat("")
 
-	pool, err := x509.SystemCertPool()
-	if err != nil {
-		panic(err)
-	}
-	testdata.AddRootCA(pool)
-	roundTripper := &http3.RoundTripper{
-		TLSClientConfig: &tls.Config{
-			RootCAs:            pool,
-			InsecureSkipVerify: *insecure,
-		},
-	}
-	defer roundTripper.Close()
-	hclient := &http.Client{
-		Transport: roundTripper,
-	}
+	// pool, err := x509.SystemCertPool()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// testdata.AddRootCA(pool)
+	// roundTripper := &http3.RoundTripper{
+	// 	TLSClientConfig: &tls.Config{
+	// 		RootCAs:            pool,
+	// 		InsecureSkipVerify: *insecure,
+	// 	},
+	// }
+	// defer roundTripper.Close()
+	// hclient := &http.Client{
+	// 	Transport: roundTripper,
+	// }
 
-	var wg sync.WaitGroup
-	wg.Add(len(urls))
-	for _, addr := range urls {
-		logger.Infof("GET %s", addr)
-		go func(addr string) {
-			rsp, err := hclient.Get(addr)
-			if err != nil {
-				panic(err)
-			}
-			logger.Infof("Got response for %s: %#v", addr, rsp)
+	// var wg sync.WaitGroup
+	// wg.Add(len(urls))
+	// for _, addr := range urls {
+	// 	logger.Infof("GET %s", addr)
+	// 	go func(addr string) {
+	// 		rsp, err := hclient.Get(addr)
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		logger.Infof("Got response for %s: %#v", addr, rsp)
 
-			body := &bytes.Buffer{}
-			_, err = io.Copy(body, rsp.Body)
-			if err != nil {
-				panic(err)
-			}
-			if *quiet {
-				logger.Infof("Request Body: %d bytes", body.Len())
-			} else {
-				logger.Infof("Request Body:")
-				logger.Infof("%s", body.Bytes())
-			}
-			wg.Done()
-		}(addr)
-	}
-	wg.Wait()
+	// 		body := &bytes.Buffer{}
+	// 		_, err = io.Copy(body, rsp.Body)
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		if *quiet {
+	// 			logger.Infof("Request Body: %d bytes", body.Len())
+	// 		} else {
+	// 			logger.Infof("Request Body:")
+	// 			logger.Infof("%s", body.Bytes())
+	// 		}
+	// 		wg.Done()
+	// 	}(addr)
+	// }
+	// wg.Wait()
 }
